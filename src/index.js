a43bf0df3be94b80239c68aa2e8532a399c8a695
chikenday <chikenday123@gmail.com>
2018-02-27 14:37:33 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  if (typeof temperature !== 'number') return 'insert number'
+
+  return temperature < -273 ? 'its absolutly zero' : temperature * 9 / 5 + 32;
 };
